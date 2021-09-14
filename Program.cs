ed677d75ad5fdcb7bbbf8170cc434737dc46186f
Sami Holmberg <shaminy1977@gmail.com>
2021-09-14 15:44:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldGitHubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
